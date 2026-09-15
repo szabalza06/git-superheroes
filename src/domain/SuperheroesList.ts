d83c65e904f5dd7361adbb149a489417fc6006b8
szabalza <santiago.zabalza06@gmail.com>
2026-09-15 19:27:07 -0300

    cambie el nombre

--- src/domain/SuperheroesList.ts
+++ src/domain/SuperheroesList.ts
@@ -7,7 +7,7 @@ export class SuperheroesList {
     { name: "Thor", side: "hero" },
     { name: "Thanos", side: "villain" },
     { name: "Loki", side: "villain" },
-    { name: "Wanda", side: "villain" },
+    { name: "Wanda Mximoff", side: "villain" },
     { name: "Ultron", side: "villain" },
     { name: "Red Skull", side: "villain" },
   ];
